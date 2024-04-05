f96bdb657cce287730938a1fe3e8d579b74c0b88
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-04-05 16:26:25 +0900

    design add

--- js/about.js
+++ js/about.js
@@ -1,4 +1,29 @@
 $(window).ready(function(){
+    //swiper js
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        speed: 2000,
+        breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 90,
+        },
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+
     var height = $(document).scrollTop();
     console.log(height);
 
@@ -10,7 +35,12 @@ $(window).ready(function(){
             $(".second_myself_wrap").css({"visibility":"visible"});
         }
 
-        if($(document).scrollTop() > 700){ 
+        if($(document).scrollTop() > 758){ 
+            $(".designP").addClass("animate__fadeIn");
+            $(".designP").css({"visibility":"visible"});
+        }
+
+        if($(document).scrollTop() > 1475){ 
             $(".bg").addClass("animate__fadeInUp");
             $(".bg").css({"visibility":"visible"});
         }
